99db24502cb3ca5afd702abf40755ed1ba46bd94
Abu-Ubaida <abuubaida901@gmail.com>
2024-12-13 18:55:09 +0500

    done with nav-bar, with center block

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+import SearchFilters from "./SearchFilters";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full top-0 left-0 py-6 border-b bg-white z-10 fixed">
+      <div className="max-w-[1500px] mx-auto px-6">
+        <div className="flex justify-between items-center">
+          <Link rel="stylesheet" href="/" >
+            <Image 
+              src="/logo.png"
+              alt='djangbnd logo'
+              width={80}
+              height={20}
+            />
+          </Link> 
+
+          <div className="flex space-x-6">
+            <SearchFilters />
+          </div>
+
+          <div className="flex items-center space-x-6">
+            Ad propary, user nav
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;

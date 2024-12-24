fb58a418a0da2d95ebaf0a83653ef329c9400957
Abu-Ubaida <abuubaida901@gmail.com>
2024-12-24 10:40:11 +0500

    done with showing the post from backend

--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -1,13 +1,47 @@
+'use client';
+import { useEffect, useState } from "react";
 import PropertyListItem from "./PropertyListItems";
 
+// this is to define the type of the property
+export type PropertyType = {
+  id: string;
+  title: string;
+  price_per_night: number;
+  image_url: string;
+}
+
 const PropertyList = () => {
+  const [properties, setProperties] = useState<PropertyType[]>([]);
+  
+  const getProperties = async () => {
+    const url = "http://127.0.0.1:8000/api/properties/";
+    
+    await fetch(url, {method: "GET", headers: {"Content-Type": "application/json"}}) 
+    .then(response => response.json())
+    .then((json) => {
+      console.log('json', json);
+      setProperties(json.data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+  };
+
+  // this is called when page is loaded or refreshed
+  useEffect(() => {
+    getProperties();
+  }, []);
+
+  
   return (
     <>
-      <PropertyListItem />
-      <PropertyListItem />
-      <PropertyListItem />
-      <PropertyListItem />
-      <PropertyListItem />
+      {properties.map((property) => (
+        <PropertyListItem 
+          key={property.id}
+          property={property}
+          />
+      ))}
     </>
   )
 }
